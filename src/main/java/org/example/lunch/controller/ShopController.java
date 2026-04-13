1120bd3eb6a43e6a196206ed44e44819437cd371
yds <yoondongseop@gmail.com>
2026-04-13 20:38:43 +0900

    error fix

--- src/main/java/org/example/lunch/controller/ShopController.java
+++ src/main/java/org/example/lunch/controller/ShopController.java
@@ -1,10 +1,11 @@
 package org.example.lunch.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.example.lunch.entity.Shop;
 import org.example.lunch.repository.ShopRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
